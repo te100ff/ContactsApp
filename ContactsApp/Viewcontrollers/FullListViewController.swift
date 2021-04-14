1edd98aa8d9cf146a1025940d020ffddaa46868e
Stanislav <te100ff@inbox.ru>
2021-04-14 10:36:01 +0300

    Images for full list

--- ContactsApp/Viewcontrollers/FullListViewController.swift
+++ ContactsApp/Viewcontrollers/FullListViewController.swift
@@ -38,15 +38,19 @@ class FullListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "info", for: indexPath)
         let person = persons[indexPath.section]
+        var content = cell.defaultContentConfiguration()
        
         if indexPath.row == 0 {
-            cell.textLabel?.text = person.phoneNumber
+            content.text = person.phoneNumber
+            content.image = UIImage(systemName: "phone")
+            
         } else {
-            cell.textLabel?.text = person.email
+            content.text = person.email
+            content.image = UIImage(systemName: "envelope")
         }
 
         
-       
+        cell.contentConfiguration = content
 
         return cell
     }
